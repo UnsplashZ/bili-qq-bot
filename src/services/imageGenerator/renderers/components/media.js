3943e8b3e6c9fc274bd8dedfc89c5ff8ff69302d
UnsplashZ <i_am_zm@qq.com>
2026-01-21 14:17:40 +0800

    v3.10.9 优化转发动态中长图显示效果

--- src/services/imageGenerator/renderers/components/media.js
+++ src/services/imageGenerator/renderers/components/media.js
@@ -11,7 +11,7 @@ const ICONS = require('../icons');
 function renderMediaHtml(images, videoCard, isOrig) {
     if (images.length === 1) {
          const style = isOrig
-            ? 'width: 100%; height: auto; object-fit: contain; margin-top: 10px;'
+            ? 'width: 100%; height: auto; margin-top: 10px;'
             : 'width: 100%; height: auto; margin-top: 20px;';
          const cls = isOrig ? 'single-image' : 'dynamic-image';
          return `<img class="${cls}" src="${images[0]}" style="${style}">`;
